d16e6e8d042bf29f4954cfc4ed102ce6d7b99a4a
wuxinbo <wuxinbo@owhat.cn>
2019-06-18 16:51:56 +0800

    添加页面

---
+++ firstapp/src/pages/me/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="users-content">
+      <div class="">
+        我的页面
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  components: {
+    card
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ firstapp/src/pages/order/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="userinfo">
+      <div class="userinfo-nickname">
+        我的订单
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  components: {
+    card
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  created () {
+  }
+}
+</script>
+
+<style>
+
+</style>
